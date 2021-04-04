886072f7a0fecfda0b1987338e2e288ba63b3e9f
Jamly Miller <jamlyyang77@gmail.com>
2021-04-04 13:45:18 -0700

    set up connection to mysql burger_db

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,20 @@
-console.log("hello burger");
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  // NOTE: Be sure to add your MySQL password here!
+  password: 'jamlyAthena789',
+  database: 'burgers_db',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+module.exports = connection;
