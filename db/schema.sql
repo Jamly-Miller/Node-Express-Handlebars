a46d12aac0eece92518b3c0951094ce4ed9d9152
Jamly Miller <jamlyyang77@gmail.com>
2021-04-05 14:12:54 -0700

    connect db to frontEnd and structured handlebars layout

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
 ### Schema
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
+
+CREATE TABLE burgers(
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(77) NOT NULL,
+	devoured BOOLEAN NOT NULL,
+	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,8 @@
-INSERT INTO customers (customers_name) VALUES ('Kevon');
-INSERT INTO customers (customers_name) VALUES ('Jane');
+USE burgers;
+INSERT INTO burgers (burger_name, devoured) VALUES ("Single Cheese Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Couple Cheese Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Sausage Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese Bacon Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Sriracha Chicken Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Roasted Chicken and Avocado Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Inspiration of The Day by Chef Burger", FALSE);
